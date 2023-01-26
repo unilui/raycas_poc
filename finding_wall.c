4e97fbd1fd86d8ced56e6ac455b2f13d2485340e
Felipe Camargo <lufelip2@student.42sp.org.br>
2023-01-26 03:49:10 -0300

    delta x/y

--- finding_wall.c
+++ finding_wall.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdio.h>
 
+#define BOX_SIZE 64
+
 #define PLAYER_Y 224
 #define PLAYER_X 96
 
@@ -23,37 +25,50 @@ double radians(int nbr)
 	return (nbr * (M_PI / 180));
 }
 
-void horizontal_check(int angle)
+int horizontal_hit(int angle)
 {
-	int	a_y;
+	int	next_box_y;
+	int	next_box_x;
+	int	delta_y;
+	int	delta_x;
+	int	direction;
+	int	correction;
 
-	if (angle < 180)
-		a_y = floor(PLAYER_Y / 64) * 64 - 1; // ray facing up
-	else
-		a_y = floor(PLAYER_Y / 64) * 64 + 64; // ray facing down
+	if (angle < 180) // ray facing up
+	{
+		correction = -1;
+		direction = -1;
+	}
+	else // ray facing down
+	{
+		correction = BOX_SIZE;
+		direction = 1;
+	}
+	delta_y = BOX_SIZE * direction;
+	delta_x = BOX_SIZE / tan(radians(angle)); // add pre build table
+	next_box_y = floor(PLAYER_Y / BOX_SIZE) * BOX_SIZE + correction;
+	next_box_x = PLAYER_X + ((PLAYER_Y - next_box_y) / tan(radians(angle)));
+	printf("A_x = %d\n", next_box_x);
+	printf("A_y = %d\n", next_box_y);
 
-
-	int a_x = PLAYER_X + ((PLAYER_Y - a_y) / tan(radians(angle)));
-	printf("A_x = %d\n", a_x / 64);
-	printf("A_y = %d\n", a_y / 64);
-	if (a_x < 0 || a_x > MAP_WIDTH - 1)
+	if (next_box_x / 64 < 0 || next_box_x / 64 > MAP_WIDTH - 1)
 		return (0);
-	if (a_y < 0 || a_y > MAP_HEIGHT - 1)
+	if (next_box_y / 64 < 0 || next_box_y / 64 > MAP_HEIGHT - 1)
 		return (0);
 }
 
-void vertical_check(int angle)
+int vertical_hit(int angle)
 {
 	int b_x;
 
 	if (angle < 90 || angle > 270)
-		b_x = floor(PLAYER_X / 64) * (64) + 64; // ray facing right
+		b_x = floor(PLAYER_X / BOX_SIZE) * (BOX_SIZE) + BOX_SIZE; // ray facing right
 	else
-		b_x = floor(PLAYER_X / 64) * (64) - 1; // ray facing left
-	printf("b_x = %d\n", b_x / 64);
+		b_x = floor(PLAYER_X / BOX_SIZE) * (BOX_SIZE) - 1; // ray facing left
+	printf("b_x = %d\n", b_x / BOX_SIZE);
 
 	int b_y = PLAYER_Y + (PLAYER_X - b_x) * tan(radians(angle));
-	printf("b_y = %d\n", b_y / 64);
+	printf("b_y = %d\n", b_y / BOX_SIZE);
 	if (b_x < 0 || b_x > MAP_WIDTH - 1)
 		return (0);
 	if (b_y < 0 || b_y > MAP_HEIGHT - 1)
@@ -64,13 +79,9 @@ int main (void)
 {
 	int degrees;
 
-	degrees = 84;
+	degrees = 60;
 	if (degrees != 0 && degrees != 180)
-	{
-		horizontal_check(degrees);
-	}
+		horizontal_hit(degrees);
 	if (degrees != 90 && degrees != 270)
-	{
-		vertical_check(degrees);
-	}
+		vertical_hit(degrees);
 }
