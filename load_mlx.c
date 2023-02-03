7e2bf363185c13a4de62cec7d7fb82c86af3abaf
Vini <vini@42sp.org.br>
2023-02-03 15:32:14 -0300

    feat: wall_pixel_put (should be working, but it ain't)

--- load_mlx.c
+++ load_mlx.c
@@ -1,7 +1,7 @@
 #include "raycast.h"
 #include <string.h>
 
-static char *walls_xpm_files[W_COUNT] = {
+static char *walls_xpm_files[S_COUNT] = {
 	"assets/no.xpm",
 	"assets/so.xpm",
 	"assets/we.xpm",
@@ -10,19 +10,21 @@ static char *walls_xpm_files[W_COUNT] = {
 
 static void	load_wall_sprite(t_screen *screen, int wall)
 {
+	t_img	*w;
 	char	*xpm_file;
 
+	w = &screen->walls[wall];
 	xpm_file = walls_xpm_files[wall];
-	screen->walls[wall].mlx_img = mlx_xpm_file_to_image(
+	w->mlx_img = mlx_xpm_file_to_image(
 			screen->mlx,
 			xpm_file,
-			&screen->walls[W_NORTH].width,
-			&screen->walls[W_NORTH].height);
-	screen->walls[wall].addr = mlx_get_data_addr(
-			screen->walls[wall].mlx_img,
-			&screen->walls[wall].bpp,
-			&screen->walls[wall].line_len,
-			&screen->walls[wall].endian);
+			&w->width,
+			&w->height);
+	w->addr = mlx_get_data_addr(
+			w->mlx_img,
+			&w->bpp,
+			&w->line_len,
+			&w->endian);
 }
 
 t_screen	load_mlx(int width, int height)
@@ -45,9 +47,9 @@ t_screen	load_mlx(int width, int height)
 			&screen.img.bpp,
 			&screen.img.line_len,
 			&screen.img.endian);
-	load_wall_sprite(&screen, W_NORTH);
-	load_wall_sprite(&screen, W_SOUTH);
-	load_wall_sprite(&screen, W_WEST);
-	load_wall_sprite(&screen, W_EAST);
+	load_wall_sprite(&screen, S_NO);
+	load_wall_sprite(&screen, S_SO);
+	load_wall_sprite(&screen, S_WE);
+	load_wall_sprite(&screen, S_EA);
 	return (screen);
 }
